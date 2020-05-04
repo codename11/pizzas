ae32d0b4e40ce1b8afd6c0a005eed867bb26822b
Veljko <veljkos82@gmail.com>
2020-05-05 00:19:55 +0200

    Try6

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'database',
             'model' => App\User::class,
         ],
 
