3daebcfece65f9920156553f66a6641f716012e8
Veljko <veljkos82@gmail.com>
2020-05-05 01:25:41 +0200

    Try12

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-    <link rel="shortcut icon" href="https://pizza.test/pizza.webp" />
+    <link rel="shortcut icon" href="{{ asset('pizza.webp') }}" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
