94b2d63af790be58bac77ede3a41a1afd25ad548
Veljko <veljkos82@gmail.com>
2020-05-05 20:32:59 +0200

    Try47

--- database/seeds/ArticlesSeeder.php
+++ database/seeds/ArticlesSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class ArticlesSeeder extends Seeder
 {
@@ -12,7 +13,8 @@ class ArticlesSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('articles')->insert(
+        $articles = [
+            
             [
                 'title' => "Italien pizza",
                 'body' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis mi congue, imperdiet nulla non, egestas magna. Phasellus posuere mauris venenatis urna euismod malesuada. Duis pharetra diam eu egestas auctor. Donec efficitur et libero ac vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris magna nibh, pharetra vel tincidunt a, efficitur a eros. Donec posuere quam a tellus posuere ultricies. Aenean scelerisque, libero id mattis ornare, neque tortor congue magna, et tempus sapien ligula quis sem. Etiam metus justo, scelerisque quis lobortis sed, dignissim sit amet eros. Aliquam a odio et augue euismod interdum vitae nec enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque nec ipsum velit. Aliquam dignissim hendrerit massa et faucibus. Etiam et est vitae quam tempus mattis. Proin sed bibendum massa, non tristique orci. Mauris quis nibh dapibus, consequat mi id, placerat sem.",
@@ -149,6 +151,9 @@ class ArticlesSeeder extends Seeder
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]
 
-    );
+        ];
+
+        DB::table('articles')->insert($articles);
+
     }
 }
