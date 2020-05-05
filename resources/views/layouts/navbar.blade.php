9b7884f98163e9051f0d3e60430f220001b0b9b1
Veljko <veljkos82@gmail.com>
2020-05-05 02:40:03 +0200

    Try18

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     <link rel="shortcut icon" href="pizza.webp" />
-
+    <!--{{ secure_asset(...) }} samo za heroku-->
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
