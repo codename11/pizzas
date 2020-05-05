9c2e97125204d94373fdf9e3a08d3b3abd521918
Veljko <veljkos82@gmail.com>
2020-05-05 16:34:03 +0200

    Try22

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -14,25 +14,25 @@
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nothing+You+Could+Do" rel="stylesheet">
 
-    <link href="{{ asset('css/open-iconic-bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/open-iconic-bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/animate.css') }}" rel="stylesheet">
 
-    <link href="{{ asset('css/owl.carousel.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/owl.theme.default.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/magnific-popup.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/owl.theme.default.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/magnific-popup.css') }}" rel="stylesheet">
 
-    <link href="{{ asset('css/aos.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/aos.css') }}" rel="stylesheet">
 
-    <link href="{{ asset('css/ionicons.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/ionicons.min.css') }}" rel="stylesheet">
 
-    <link href="{{ asset('css/bootstrap-datepicker.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/jquery.timepicker.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/bootstrap-datepicker.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/jquery.timepicker.css') }}" rel="stylesheet">
 
     
-    <link href="{{ asset('css/flaticon.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/icomoon.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ secure_asset('css/flaticon.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/icomoon.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
     
   </head>
   <body data-spy="scroll" data-target=".ftco_navbar" data-offset="160">
@@ -669,23 +669,23 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
-<!--<script src="{{ asset('js/jquery.min.js') }}" defer></script>-->
-<script src="{{ asset('js/jquery-migrate-3.0.1.min.js') }}" defer></script>
-<!--<script src="{{ asset('js/popper.min.js') }}" defer></script>-->
-<!--<script src="{{ asset('js/bootstrap.min.js') }}" defer></script>-->
-<script src="{{ asset('js/jquery.easing.1.3.js') }}" defer></script>
-<script src="{{ asset('js/jquery.waypoints.min.js') }}" defer></script>
-<script src="{{ asset('js/jquery.stellar.min.js') }}" defer></script>
-<script src="{{ asset('js/owl.carousel.min.js') }}" defer></script>
-<script src="{{ asset('js/jquery.magnific-popup.min.js') }}" defer></script>
-<script src="{{ asset('js/aos.js') }}" defer></script>
-<script src="{{ asset('js/jquery.animateNumber.min.js') }}" defer></script>
-<script src="{{ asset('js/bootstrap-datepicker.js') }}" defer></script>
-<script src="{{ asset('js/jquery.timepicker.min.js') }}" defer></script>
-<script src="{{ asset('js/scrollax.min.js') }}" defer></script>
+<!--<script src="{{ secure_asset('js/jquery.min.js') }}" defer></script>-->
+<script src="{{ secure_asset('js/jquery-migrate-3.0.1.min.js') }}" defer></script>
+<!--<script src="{{ secure_asset('js/popper.min.js') }}" defer></script>-->
+<!--<script src="{{ secure_asset('js/bootstrap.min.js') }}" defer></script>-->
+<script src="{{ secure_asset('js/jquery.easing.1.3.js') }}" defer></script>
+<script src="{{ secure_asset('js/jquery.waypoints.min.js') }}" defer></script>
+<script src="{{ secure_asset('js/jquery.stellar.min.js') }}" defer></script>
+<script src="{{ secure_asset('js/owl.carousel.min.js') }}" defer></script>
+<script src="{{ secure_asset('js/jquery.magnific-popup.min.js') }}" defer></script>
+<script src="{{ secure_asset('js/aos.js') }}" defer></script>
+<script src="{{ secure_asset('js/jquery.animateNumber.min.js') }}" defer></script>
+<script src="{{ secure_asset('js/bootstrap-datepicker.js') }}" defer></script>
+<script src="{{ secure_asset('js/jquery.timepicker.min.js') }}" defer></script>
+<script src="{{ secure_asset('js/scrollax.min.js') }}" defer></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBNhe45otXCcDn2ZV1dqla-IyYTyLm1HDU&sensor=false"></script>
-<script src="{{ asset('js/google-map.js') }}" defer></script>
-<script src="{{ asset('js/main.js') }}" defer></script>
+<script src="{{ secure_asset('js/google-map.js') }}" defer></script>
+<script src="{{ secure_asset('js/main.js') }}" defer></script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" defer></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js" defer></script>
