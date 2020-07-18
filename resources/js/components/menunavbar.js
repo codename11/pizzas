1ba5bde98262d05e60fb8d45d229de4fe1345ed3
Veljko <veljkos82@gmail.com>
2020-07-18 22:21:55 +0200

    Fixed mobile menu not closing  by reinstalling jquery and updating npm.

--- resources/js/components/menunavbar.js
+++ resources/js/components/menunavbar.js
@@ -61,12 +61,12 @@ class MenuNavbar extends React.Component {
     }
 
     componentDidMount(){
-        
+
         document.addEventListener("click", (event) => {
             
             let elem = event.target;
-            let tag = event.target.tagName;
-            let klasa = event.target.className;
+            let tag = elem.tagName;
+            let klasa = elem.className;
             let href = null;
             let page = null;
             
@@ -82,12 +82,6 @@ class MenuNavbar extends React.Component {
                     page: page
                 }, this.listArticles(null, page));
                 console.log(this.state);
-
-            }
-
-            if(tag==="a" && klasa.indexOf("nav-link")>-1){
-
-                $("button[data-toggle=collapse").click();
 
             }
 
